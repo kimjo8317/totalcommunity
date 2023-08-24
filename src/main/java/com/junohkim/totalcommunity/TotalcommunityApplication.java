edae96bb9def043db359eb0873a212de69b02263
kimjunoh <kimjo8317@gmail.com>
2023-08-24 15:15:05 +0900

    first commit

---
+++ src/main/java/com/junohkim/totalcommunity/TotalcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.junohkim.totalcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junohkim/totalcommunity/TotalcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junohkim.totalcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
