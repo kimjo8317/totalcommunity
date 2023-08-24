38d050c9b8e040dfba8e4c31c790ee13526f7de0
kimjunoh <kimjo8317@gmail.com>
2023-08-25 00:09:08 +0900

    데이터 크롤링

---
+++ src/main/java/com/junohkim/totalcommunity/controller/NewsController.java
@@ -0,0 +1,27 @@
+package com.junohkim.totalcommunity.controller;
+
+import com.junohkim.totalcommunity.News;
+import com.junohkim.totalcommunity.service.NewsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class NewsController {
+
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping("/news")
+    public String news(Model model) throws Exception{
+        List<News> newsList = newsService.getNewsDatas();
+        model.addAttribute("news", newsList);
+
+        return "news";
+    }
+}
